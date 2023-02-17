80261b619811290c7bd8926ff4cc928e28cb30b2
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-02-17 12:05:32 +0530

    completed custom button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Button from "./Button";
+import { GoBell } from "react-icons/go";
 
 function App() {
-  return <div>App</div>;
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <div>
+      <div>
+        <Button primary={true} className="mb-5" onClick={handleClick}>
+          <GoBell />
+          Ring Bell
+        </Button>
+      </div>
+      <div>
+        <Button success outline rounded>
+          Buy
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
