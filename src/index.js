9a97b1a7613c01a2502cbf5d7484a7a2d0a13301
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-02-21 00:03:02 +0530

    navigation context provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import ReactDOM from "react-dom/client";
-
+import { NavigationProvider } from "./context/navigation";
 import App from "./App";
-import './index.css';
+import "./index.css";
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
